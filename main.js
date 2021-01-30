40634d136b3fc8c5d2cd985955c4c1a836695340
わたるンゴ <72363964+massswwwa@users.noreply.github.com>
2021-01-31 01:34:10 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 function randomValueFromArray(array) {
-    const random = Math.floor(Math.random() * array.length);
+    const random = Math.floor(Math.random()*array.length);
     return array[random];
 }
 
@@ -22,18 +22,18 @@ function result() {
     let zItem = randomValueFromArray(insertZ);
 
     newStory = newStory.replace(':insertx:',xItem);
-    newStory = newStory.replace(':insertx:',xItem);
     newStory = newStory.replace(':inserty:',yItem);
     newStory = newStory.replace(':insertz:',zItem);
+    newStory = newStory.replace(':insertx:',xItem);
 
     if (customName.value !== '') {
-        const name = customName.value;
+        let name = customName.value;
         newStory = newStory.replace('Bob',name);
     }
 
     if (document.getElementById("uk").checked) {
-        const weight = Math.round(300 * 0.0714286) + ' stone';
-        const temperature = Math.round((94-32)*5/9) + ' centigrade';
+        let weight = Math.round(300*0.0714286) + ' stone';
+        let temperature = Math.round((94-32)*(5/9)) + ' centigrade';
         newStory = newStory.replace('94 fahrenheit',temperature);
         newStory = newStory.replace('300 pounds',weight);
     }
